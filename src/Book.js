0c2548961ed42d52e8625bf746efcccd8dd5f16e
Clara Wong <cwcawa@gmail.com>
2018-01-21 18:30:00 -0500

    Fix searchings books show missing none in action

--- src/Book.js
+++ src/Book.js
@@ -27,7 +27,7 @@ class Book extends React.Component {
           </div>
           <div className="book-shelf-changer">
             <select value={shelf} onChange={(event) => this.handleSelectedOption(event)}>
-              <option value="none" disabled>Move to...</option>
+              <option value="moveTo" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
